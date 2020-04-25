e39f734c6bcaf4f7168bd91d6e6d399c1132d2ec
hadxu <hadxu123@gmail.com>
2020-04-25 12:33:08 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
--------------------------------------------------
-   File Name：     setup.py
-   Description :
-   Author :       haxu
-   date：          2020-04-25
--------------------------------------------------
-   Change Activity:
-                   2020-04-25:
--------------------------------------------------
-"""
-__author__ = 'haxu'
+import setuptools
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="micrograd",
+    version="0.1.0",
+    author="hadxu",
+    author_email="hadxu123@gmail.com",
+    description="Autograd",
+    readme=readme,
+    readme_type="text/markdown",
+    url="https://github.com/hadxu/micrograd",
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires='>=3.6',
+)
